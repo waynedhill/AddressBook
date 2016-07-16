7dbb4a6d96b0aa74fcec90091e517087757dbfcf
Wayne Hill <waynedhill@live.com>
2016-07-16 18:45:56 -0400

    Added to source

---
+++ DetailViewController.swift
@@ -0,0 +1,46 @@
+//
+//  DetailViewController.swift
+//  AddressBook
+//
+//  Created by Wayne Hill on 7/13/16.
+//  Copyright © 2016 Surfside Software Solutions. All rights reserved.
+//
+
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+  @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+  var detailItem: AnyObject? {
+    didSet {
+        // Update the view.
+        self.configureView()
+    }
+  }
+
+  func configureView() {
+    // Update the user interface for the detail item.
+    if let detail = self.detailItem {
+        if let label = self.detailDescriptionLabel {
+            label.text = detail.valueForKey("timeStamp")!.description
+        }
+    }
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    self.configureView()
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
